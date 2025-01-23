d097354be9f028eacfa8f20b9a4bc3f683bad08c
Laura de Koning <lade-kon@student.codam.nl>
2025-01-23 16:24:17 +0100

    Refactor monitoring logic for improved clarity and modularity
    - Renamed `all_philos_full` to `everyone_full` for clearer naming consistency.
    - Introduced `philo_died` and `someone_died` functions to encapsulate death-checking logic, reducing redundancy.
    - Moved `write_status` calls into the newly created `someone_died` function to centralize action logging for philosopher death.
    - Reorganized the `monitor` function to streamline the main simulation loop, ensuring easier readability and maintenance.
    - Added a small sleep (`usleep(500)`) in the monitoring loop to optimize CPU usage.

--- philo/src/monitor.c
+++ philo/src/monitor.c
@@ -6,13 +6,13 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/23 16:01:39 by lade-kon      #+#    #+#                 */
-/*   Updated: 2025/01/23 16:07:29 by lade-kon      ########   odam.nl         */
+/*   Updated: 2025/01/23 16:23:01 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-bool	all_philos_full(t_table *table)
+bool	everyone_full(t_table *table)
 {
 	size_t	i;
 	t_philo	*philo;
@@ -28,31 +28,47 @@ bool	all_philos_full(t_table *table)
 	return (true);
 }
 
-bool	is_philo_dead(t_table *table, size_t time, size_t i)
+bool	philo_died(t_table *table, size_t i)
 {
 	if (table->philos[i].last_meal_time == 0)
 	{
 		if ((time - table->start_simulation) > table->time_to_die)
-		{
-			write_status(DIED, &table->philos[i]);
 			return (true);
-		}
 	}
 	else
 	{
 		if ((time - table->philos[i].last_meal_time) > table->time_to_die)
-		{
-			write_status(DIED, &table->philos[i]);
 			return (true);
-		}
 	}
 	return (false);
 }
 
-void	*monitor(t_table *table)
+bool	someone_died(t_table *table)
 {
 	size_t	time;
 	size_t	i;
+
+	i = 0;
+	while (i < table->philo_count)
+	{
+		mutex_handle(&table->philos[i].philo_mutex, LOCK);
+		time = gettime();
+		if (philo_died(table, i) == true)
+		{
+			write_status(DIED, &table->philos[i]);
+			set_bool(&table->table_mutex, &table->death, true);
+			set_bool(&table->prog_m[STOP], &table->end_simulation, true);
+			mutex_handle(&table->philos[i].philo_mutex, UNLOCK);
+			break ;
+		}
+		mutex_handle(&table->philos[i].philo_mutex, UNLOCK);
+		i++;
+	}
+
+}
+
+void	*monitor(t_table *table)
+{
 
 	while (1)
 	{
@@ -60,25 +76,11 @@ void	*monitor(t_table *table)
 			return (NULL);
 		if (everyone_full(table))
 			return (NULL);
+		usleep(500);
 	}
 
 	while (table->end_simulation == false)
 	{
-		i = 0;
-		while (i < table->philo_count)
-		{
-			mutex_handle(&table->philos[i].philo_mutex, LOCK);
-			time = gettime();
-			if (is_philo_dead(table, time, i) == true)
-			{
-				set_bool(&table->table_mutex, &table->death, true);
-				set_bool(&table->prog_m[STOP], &table->end_simulation, true);
-				mutex_handle(&table->philos[i].philo_mutex, UNLOCK);
-				break ;
-			}
-			mutex_handle(&table->philos[i].philo_mutex, UNLOCK);
-			i++;
-		}
 		if (all_philos_full(table) == true)
 			set_bool(&table->prog_m[STOP], &table->end_simulation, true);
 	}
